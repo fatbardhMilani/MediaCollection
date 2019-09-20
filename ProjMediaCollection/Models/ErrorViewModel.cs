77e5cf7fdb951bb20ec57eafed0c5a1d2794d05a
Student Education <Student@rdeducation.be>
2019-09-20 10:14:39 +0200

    Add project files.

---
+++ ProjMediaCollection/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjMediaCollection.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
